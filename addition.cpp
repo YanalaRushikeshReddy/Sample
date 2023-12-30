bd5a89315d9fd979bc7c062879031c563c6f395d
Yanala Rushikesh Reddy <121600479+Rishi1236@users.noreply.github.com>
2023-12-30 10:13:07 +0530

    Add files via upload

--- addition.cpp
+++ addition.cpp
@@ -5,15 +5,15 @@
 #define SIZE 40
 
 struct queue {
-  int items[SIZE;
+  int items[SIZE];
   int front;
   int rear;
-;
+};
 
 struct queue* createQueue();
 void enqueue(struct queue* q, int);
 int dequeue(struct queue* q);
-void display(struct queue q);
+void display(struct queue* q);
 int isEmpty(struct queue* q);
 void printQueue(struct queue* q);
 
